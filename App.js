cdbce0d456891ae3f7daf018c3a70c132b6f0814
thaovy <thaovyluuthanh@gmail.com>
2022-05-23 19:13:38 +0700

    update

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import HomeScreen from './screen/home/HomeScreen'
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  )
+}
